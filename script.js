94c4fe6b810a491a4da69887100e581d25c38865
Rohitha Sresta Ganji <rohithasresta@Rohithas-MacBook-Air.local>
2024-07-28 11:39:36 +0530

    first version

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "share a fact";
+  }
+});
+
+let votesInteresting = 23;
+let votesMindblowing = 5;
+const text = "Lisbon is the capital of portugal";
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+let totalUpvotes = votesInteresting + votesMindblowing;
+console.log("Upvotes:", totalUpvotes);
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
